74f48be5ce8717f0bf1cd80a6f905f3725409355
hyuntony <ankim90@gmail.com>
2021-06-04 09:03:52 -0400

    finished with promises

---
+++ iss_promised.js
@@ -0,0 +1,29 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const data = JSON.parse(body);
+  const latitude = data.latitude;
+  const longitude = data.longitude;
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`);
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
