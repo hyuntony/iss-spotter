94f20f576299eb29fe38f03debcd482b86f5d238
hyuntony <ankim90@gmail.com>
2021-06-04 06:16:14 -0400

    added getcoordbyip function

--- iss.js
+++ iss.js
@@ -24,5 +24,26 @@ const fetchMyIP = function(callback) {
   });
 };
 
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response : ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
 
-module.exports = { fetchMyIP };
+    const data = JSON.parse(body);
+    const coordinates = {
+      latitude: data.latitude,
+      longitude: data.longitude
+    };
+    callback(null, coordinates);
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
